49171c99486957a7a7e26f3f7f067e099a42e75c
mokv <crispy9889@gmail.com>
2017-11-04 15:07:44 +0200

    Homework for sixth week

---
+++ Homeworks/STD1A-AsenRusev-1701681005-H6/GeometryCalculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static double CalculateTriangleArea()
+        {
+            Console.WriteLine("Enter side: ");
+            double side = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height: ");
+            double height = double.Parse(Console.ReadLine());
+            return (side * height) / 2;
+        }
+
+        static double CalculateSquareArea()
+        {
+            Console.WriteLine("Enter side: ");
+            double side = double.Parse(Console.ReadLine());
+            return side * side;
+        }
+
+        static double CalculateRadians()
+        {
+            Console.WriteLine("Enter degrees: ");
+            double degrees = double.Parse(Console.ReadLine());
+            return (Math.PI / 180) * degrees;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool successful = true;
+                do
+                {
+                    successful = true;
+                    Console.WriteLine("Choose option (triangle, square or radians): ");
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "triangle":
+                            Console.WriteLine("The are of the triangle is = {0}", CalculateTriangleArea().ToString("#0.00"));
+                            break;
+                        case "square":
+                            Console.WriteLine("The are of the square is = {0}", CalculateSquareArea().ToString("#0.00"));
+                            break;
+                        case "radians":
+                            Console.WriteLine("Your degrees in radians are = {0}", CalculateRadians());
+                            break;
+                        default:
+                            successful = false;
+                            break;
+                    }
+                } while (!successful);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Homeworks/STD1A-AsenRusev-1701681005-H6/GetMax/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static int GetMax(int first, int second, int third)
+        {
+            int max = int.MinValue;
+
+            if(first > max)
+            {
+                max = first;
+            }
+
+            if(second > max)
+            {
+                max = second;
+            }
+
+            if(third > max)
+            {
+                max = third;
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first number: ");
+                int first = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number: ");
+                int second = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter third number: ");
+                int third = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"The biggest number is {GetMax(first, second, third)}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
